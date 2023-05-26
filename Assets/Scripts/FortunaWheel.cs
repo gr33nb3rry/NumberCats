91979f6562e42536dd69e6a717713887b2d86e20
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2023-05-26 23:07:04 +0300

    FortunaWheel (update rewardTexts, Shuffle rewards every spin)

--- Assets/Scripts/FortunaWheel.cs
+++ Assets/Scripts/FortunaWheel.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,29 +12,46 @@ public class FortunaWheel : MonoBehaviour
     public GameObject spinButton;
     public RectTransform wheelCells;
     public GameObject wheel;
+    public Text[] wheelRewardsText;
 
     public Text rewardText;
     public Image rewardImage;
     public AudioClip winSound;
     public AudioSource audioSource;
     private int reward;
+    private int[] wheelRewards = new int[] {300,1,150,2,50,3,10,4};
     public void OnEnable()
     {
         spinButton.SetActive(true);
         wheelCells.rotation = Quaternion.identity;
+        UpdateRewards();
+    }
+    private void UpdateRewards()
+    {
+        for (int i = 0; i < wheelRewardsText.Length; i++)
+        {
+            wheelRewardsText[i].text = wheelRewards[i].ToString();
+        }
     }
     public void Spin()
     {
         wheel.GetComponent<Animation>().Play();
         spinButton.SetActive(false);
+        Invoke("Mix", 1);
+    }
+    private void Mix()
+    {
+        System.Random random = new System.Random();
+        wheelRewards = wheelRewards.OrderBy(x => random.Next()).ToArray();
+        UpdateRewards();
     }
     private void End()
     {
     }
     public void Collect()
     {
-        Menu.coins += reward;
         menu.UpdateCoins();
+        menu.UpdateLifes();
         endpanel.SetActive(false);
         gameObject.SetActive(false);
         challengesPanel.SetActive(false);
