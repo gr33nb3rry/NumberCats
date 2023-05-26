8a8cd3c5830bba9129150b94498767f62d8bd33e
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2023-05-26 23:39:14 +0300

    обновил перемешку (mixImagesToLifes не роибт)

--- Assets/Scripts/FortunaWheel.cs
+++ Assets/Scripts/FortunaWheel.cs
@@ -13,24 +13,29 @@ public class FortunaWheel : MonoBehaviour
     public RectTransform wheelCells;
     public GameObject wheel;
     public Text[] wheelRewardsText;
+    public Image[] wheelRewardsImages;
+    public Sprite[] coinsLifesImages;
 
     public Text rewardText;
     public Image rewardImage;
     public AudioClip winSound;
     public AudioSource audioSource;
     private int reward;
-    private int[] wheelRewards = new int[] {300,1,150,2,50,3,10,4};
+    private int[] wheelRewardsCoins = new int[] {300,150,50,10};
+    private int[] wheelRewardsLifes = new int[] {1, 2, 3, 4};
     public void OnEnable()
     {
         spinButton.SetActive(true);
         wheelCells.rotation = Quaternion.identity;
         UpdateRewards();
+        MixImagesToCoin();
     }
     private void UpdateRewards()
     {
-        for (int i = 0; i < wheelRewardsText.Length; i++)
+        for (int i = 0; i < wheelRewardsText.Length / 2; i++)
         {
-            wheelRewardsText[i].text = wheelRewards[i].ToString();
+            wheelRewardsText[i * 2].text = wheelRewardsLifes[i].ToString();
+            wheelRewardsText[i * 2 + 1].text = wheelRewardsCoins[i].ToString();
         }
     }
     public void Spin()
@@ -38,15 +43,49 @@ public class FortunaWheel : MonoBehaviour
         wheel.GetComponent<Animation>().Play();
         spinButton.SetActive(false);
         Invoke("Mix", 1);
+        Invoke("Win", 5);
     }
     private void Mix()
     {
         System.Random random = new System.Random();
-        wheelRewards = wheelRewards.OrderBy(x => random.Next()).ToArray();
+        int isRewardCoins = Random.Range(0, 2);
+        wheelRewardsCoins = wheelRewardsCoins.OrderBy(x => random.Next()).ToArray();
+        wheelRewardsLifes = wheelRewardsLifes.OrderBy(x => random.Next()).ToArray();
+
         UpdateRewards();
+        Debug.Log(isRewardCoins);
+        if (isRewardCoins == 1)
+        {
+            MixImagesToCoin();
+        }
+        else
+        {
+            MixImagesToLife();
+        }
+    }
+    private void MixImagesToCoin()
+    {
+        for (int i = 0; i < wheelRewardsImages.Length / 2; i++)
+        {
+            wheelRewardsImages[i * 2].sprite = coinsLifesImages[1];
+            wheelRewardsImages[i * 2 + 1].sprite = coinsLifesImages[0];
+        }
+    }
+    private void MixImagesToLife()
+    {
+        for (int i = 0; i < wheelRewardsImages.Length / 2; i++)
+        {
+            wheelRewardsImages[i * 2].sprite = coinsLifesImages[0];
+            wheelRewardsImages[i * 2 + 1].sprite = coinsLifesImages[1];
+        }
+    }
+    private void Win()
+    {
+        //reward = wheelRewards[2];
     }
     private void End()
     {
+
     }
     public void Collect()
     {
